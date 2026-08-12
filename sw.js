55d97189b51debb8e25a9151c22b0122f796acbd
mateoespinosag-pixel <mateoespinosag@gmail.com>
2026-08-12 09:23:43 -0500

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='phisique-q3-v1';
+const ASSETS=['./','./index.html','./data1.js','./data2.js','./data_rest.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
